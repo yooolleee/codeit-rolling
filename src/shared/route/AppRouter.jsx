8dd535661c934f3e3960013ecb1b828a71104fcf
jieunsse <jieunsse@gmail.com>
2025-11-04 00:21:20 +0900

    chore : 초기 설정 업로드

---
+++ src/shared/route/AppRouter.jsx
@@ -0,0 +1,12 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from '../../pages/home/Home.jsx';
+
+export default function AppRouter() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
