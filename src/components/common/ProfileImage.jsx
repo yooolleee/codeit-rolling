87b72d4c89e9742874bad4905fb63e0c9b4b3e3d
jangseoyeong <suhyoung030327@naver.com>
2025-11-06 14:19:36 +0900

    Feat: 프로필 이미지 컴포넌트 생성

---
+++ src/components/common/ProfileImage.jsx
@@ -0,0 +1,13 @@
+import styles from './ProfileImage.module.css';
+
+function ProfileImage({profileImageUrl}) {
+  const src = profileImageUrl ;
+
+  return (
+    <div className={styles.container}>
+      <img src={src} alt='프로필 이미지'/>
+    </div>
+  )
+}
+
+export default ProfileImage;
