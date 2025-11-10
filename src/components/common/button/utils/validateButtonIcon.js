b3d594f5a9aa815b04304f95032aa905551f460b
jieunsse <jieunsse@gmail.com>
2025-11-10 23:41:19 +0900

    Chore : 매직넘버 상수화 #11

--- src/components/common/button/utils/validateButtonIcon.js
+++ src/components/common/button/utils/validateButtonIcon.js
@@ -1,5 +1,6 @@
 export function validateButtonIcon({ variant, size, isIcon }) {
-  if (variant === 'outlined' && size === 56 && isIcon) {
+  const OUTLINED_SIZE = 56;
+  if (variant === 'outlined' && size === OUTLINED_SIZE && isIcon) {
     throw new Error('outlined 56 사이즈에서는 아이콘을 사용할 수 없습니다.');
   }
 }
