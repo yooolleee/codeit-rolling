2face74e893b8c0bb91f0dde483426045adc4609
seoyoung <nct830@naver.com>
2025-11-12 20:56:12 +0900

    Feat : 버튼 컴포넌트 디자인 state로 변경 적용하기

--- src/components/modal/ModalFooter.jsx
+++ src/components/modal/ModalFooter.jsx
@@ -1,7 +1,10 @@
+import { useState } from 'react';
 import styles from './ModalFooter.module.css';
 import Button from '@components/common/button/base/Button.jsx';
 
 function ModalFooter({ onClose }) {
+  const [visualState, setVisualState] = useState('enabled');
+
   return (
     <>
       <div className={styles.footer}>
@@ -9,7 +12,13 @@ function ModalFooter({ onClose }) {
           onClick={onClose}
           buttonName="확인"
           variant="primary"
-          visualState="enabled"
+          visualState={visualState}
+          onMouseEnter={() => setVisualState('hover')}
+          onMouseLeave={() => setVisualState('enabled')}
+          onMouseDown={() => setVisualState('pressed')}
+          onMouseUp={() => setVisualState('hover')}
+          onFocus={() => setVisualState('focus')}
+          onBlur={() => setVisualState('enabled')}
         />
       </div>
     </>
