9604d7b183ebdac41ca211d6dafdb16889c8fb2a
jieunsse <jieunsse@gmail.com>
2025-11-13 22:18:46 +0900

    Fix : 리액션 뱃지 컨플릭트 충돌

--- src/components/common/badge/relationBadge/RelationBadge.jsx
+++ src/components/common/badge/relationBadge/RelationBadge.jsx
@@ -2,21 +2,9 @@ import styles from '@components/common/badge/relationBadge/relationBadge.module.
 import { validateBadgeName } from '@components/common/badge/util/validateBadgeName.js';
 import { nameMap } from '@components/common/badge/relationBadge/type/nameMap.js';
 
-<<<<<<< HEAD
-<<<<<<< HEAD
-export default function RelationBadge({ badgeName }) {
-  validateBadgeName(badgeName);
-
-  const key = nameMap[badgeName];
-  const colorClass = styles[key];
-
-  return <div className={`${styles.badge} ${colorClass}`}>{badgeName}</div>;
-=======
-=======
 /**
  * @param {string} title - 뱃지 이름(색상 매핑에 사용됨)
  */
->>>>>>> origin/dev
 export default function RelationBadge({ title }) {
   validateBadgeName(title);
 
@@ -24,5 +12,4 @@ export default function RelationBadge({ title }) {
   const colorClass = styles[key];
 
   return <div className={`${styles.badge} ${colorClass}`}>{title}</div>;
->>>>>>> dev
 }
